53bcad13942e311e1bfe59632d3acc8612df4321
Nidheesh K V <nidheeshkv@solutech-soft.com>
2025-09-20 20:01:36 +0530

    Added feature1

--- app/src/main/java/com/example/cicddemoapp/MainActivity.kt
+++ app/src/main/java/com/example/cicddemoapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CICDDemoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android CI/CD Demo",
+                        name = "Android CI/CD Demo first feature",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
